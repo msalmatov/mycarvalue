740867be570750216b2bc0828803ada34ec519a9
Murod <almatov.ms@gmail.com>
2023-08-14 11:30:27 +0500

    create interceptor to serialize output data

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
